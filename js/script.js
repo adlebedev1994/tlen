39cb0579be4e62eccd4cdd90e20e842e9cd5aef2
adlebedev1994 <98585299+adlebedev1994@users.noreply.github.com>
2022-04-02 00:28:51 +0300

    load 4

--- js/script.js
+++ js/script.js
@@ -6,8 +6,8 @@ var days = Math.floor(distance / (1000 * 60 * 60 * 24));
 var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
 var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
 var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-document.getElementById("demo").innerHTML = days + "-" + hours + ":"
-  + minutes + "m " + seconds + "s ";
+document.getElementById("demo").innerHTML = days + "d:" + hours + "h:"
+  + minutes + "m:" + seconds + "s";
     
 if (distance < 0) {
     clearInterval(x);
