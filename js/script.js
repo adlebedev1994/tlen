fd50134c9d207632a9594d2c376a1734cf9604e9
adlebedev1994 <98585299+adlebedev1994@users.noreply.github.com>
2022-04-02 20:43:11 +0300

    load 2
    load 2

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("Apr 2, 2022 08:30:00").getTime();
+var countDownDate = new Date("Apr 8, 2022 08:30:00").getTime();
 var x = setInterval(function() {
 var now = new Date().getTime();
 var distance = countDownDate - now;
